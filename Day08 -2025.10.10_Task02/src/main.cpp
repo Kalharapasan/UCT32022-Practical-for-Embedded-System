0f7115290791bccf3a3ab267b58ab348d200ef59
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-10-10 11:08:19 +0530

    Today Work (2025.10.10)-Task02(ultrasonic sensor with display)

---
+++ Day08 -2025.10.10_Task02/src/main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+const int ECHO_PIN = 9;
+const int TRIG_PIN = 8;
+
+float readDistanceCM() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  int duration = pulseIn(ECHO_PIN, HIGH);
+  return duration * 0.034 / 2;
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+
+}
+
+void loop() {
+  float distance = readDistanceCM();
+  Serial.print("Measured distance: ");
+  Serial.println(readDistanceCM());
+  lcd.setCursor(16, 0);   
+  lcd.print("Measured distance:");
+  lcd.print(distance);
+  delay(100);
+}
