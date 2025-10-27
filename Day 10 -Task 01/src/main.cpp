68411fe3f18aed46b79ecd162ca0e341a1b95f51
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-10-27 09:03:30 +0530

    Day10 -2025.10.27_Task01

--- Day 10 -Task 01/src/main.cpp
+++ Day 10 -Task 01/src/main.cpp
@@ -1,14 +1,10 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 
-#define I2C_ADDR    0x27
-#define LCD_COLUMNS 20
-#define LCD_LINES   4
-
 const int ledPin= 13;
 const int pirPin = 2;
 
-LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_LINES);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
  
