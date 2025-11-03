acafbf1dcfce1c25825225557a058a22093721e6
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-11-03 09:49:20 +0530

    Day12 -2025.10.31_Task01

--- Day12 -2025.10.31_Task01/src/main.cpp
+++ Day12 -2025.10.31_Task01/src/main.cpp
@@ -1,18 +1,52 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int xdire = A0;
+int ydire = A1;
+
+int lt = 2, lb = 3, ll = 4, lr = 5;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(lt, OUTPUT);
+  pinMode(lb, OUTPUT);
+  pinMode(ll, OUTPUT);
+  pinMode(lr, OUTPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  int xValue = analogRead(xdire);
+  int yValue = analogRead(ydire);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Define a dead zone range around 512 (center)
+  int deadZone = 100; // Adjust if needed
+
+  // Default: turn off all LEDs
+  digitalWrite(lt, LOW);
+  digitalWrite(lb, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lr, LOW);
+
+  // Check X and Y directions
+  if (xValue < (512 - deadZone)) {
+    digitalWrite(ll, HIGH);
+    Serial.println("Left");
+  } 
+  else if (xValue > (512 + deadZone)) {
+    digitalWrite(lr, HIGH);
+    Serial.println("Right");
+  } 
+  else if (yValue < (512 - deadZone)) {
+    digitalWrite(lt, HIGH);
+    Serial.println("Top");
+  } 
+  else if (yValue > (512 + deadZone)) {
+    digitalWrite(lb, HIGH);
+    Serial.println("Bottom");
+  } 
+  else {
+    Serial.println("Center");
+  }
+
+  delay(200);
 }
