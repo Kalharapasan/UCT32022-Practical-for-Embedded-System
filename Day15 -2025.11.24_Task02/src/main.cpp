d3bbbf3b9174f3e3c47081e85110254adbdd3c65
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-11-24 09:13:38 +0530

    Day15 -2025.11.24_Task02

--- Day15 -2025.11.24_Task02/src/main.cpp
+++ Day15 -2025.11.24_Task02/src/main.cpp
@@ -1,18 +1,29 @@
 #include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <LiquidCrystal_I2C.h>
 
-// put function declarations here:
-int myFunction(int, int);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+#define ONE_WIRE_BUS 2
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup(void)
+{
+  sensors.begin();
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Temperature:");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop(void){ 
+  sensors.requestTemperatures(); 
+  lcd.setCursor(0,1);
+  lcd.print(sensors.getTempCByIndex(0));
+  lcd.print(" C ");
+  lcd.print(sensors.getTempFByIndex(0));
+  lcd.print(" F ");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(1000);
 }
