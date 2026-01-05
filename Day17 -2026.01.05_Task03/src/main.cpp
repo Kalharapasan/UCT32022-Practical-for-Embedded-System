52a2061ca02c130ddf2b177c3e0a5324bf4ac60b
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-01-05 09:19:55 +0530

    Day17 -2026.01.05_Task03

--- Day17 -2026.01.05_Task03/src/main.cpp
+++ Day17 -2026.01.05_Task03/src/main.cpp
@@ -1,18 +1,122 @@
 #include <Arduino.h>
+#include <IRremote.hpp>
+#include <LiquidCrystal_I2C.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define IR_RECEIVE_PIN 13
+#define I2C_ADDR    0x27
+#define LCD_COLUMNS 20
+#define LCD_LINES   4
+
+int redledPin =2;
+int greenledPin =3;
+int blueledPin =4;
+int buzzerPin =5;
+
+LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_LINES);
+
+// Function declarations
+void translateIR();
+void lcdPrint(const char* text);
+
+void initializeReceiver() {
+  // set up the receiver to receive input the NEW way- it changed from earlier versions)
+  IrReceiver.begin(IR_RECEIVE_PIN);
+}
+
+void initializeLCD() {
+  lcd.begin(LCD_COLUMNS, LCD_LINES);
+  lcd.print("<press a button>");  // print something.
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  initializeReceiver();
+  initializeLCD();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Checks received an IR signal
+  if (IrReceiver.decode()) {
+    translateIR(); // calls our translate function
+    IrReceiver.resume();  // Receive the next value
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void lcdPrint(const char* text) {
+  lcd.clear();
+  lcd.setCursor(0, 0); // set cursor for text to print in top left row/column
+  lcd.print("button pressed:");
+  lcd.setCursor(0, 1); // move cursor to print to next line.
+  lcd.print(text);
+  lcd.print(" code: ");
+  lcd.print(IrReceiver.decodedIRData.command);
+}
+
+void translateIR() {
+  // Takes command based on IR code received
+  switch (IrReceiver.decodedIRData.command) {
+    case 162:
+      lcdPrint("POWER");
+      break;
+    case 226:
+      lcdPrint("MENU");
+      break;
+    case 34:
+      lcdPrint("TEST");
+      break;
+    case 2:
+      lcdPrint("PLUS");
+      break;
+    case 194:
+      lcdPrint("BACK");
+      break;
+    case 224:
+      lcdPrint("PREV.");
+      break;
+    case 168:
+      lcdPrint("PLAY");
+      break;
+    case 144:
+      lcdPrint("NEXT");
+      break;
+    case 104:
+      lcdPrint("num: 0");
+      break;
+    case 152:
+      lcdPrint("MINUS");
+      break;
+    case 176:
+      lcdPrint("key: C");
+      break;
+    case 48:
+      lcdPrint("num: 1");
+      break;
+    case 24:
+      lcdPrint("num: 2");
+      break;
+    case 122:
+      lcdPrint("num: 3");
+      break;
+    case 16:
+      lcdPrint("num: 4");
+      break;
+    case 56:
+      lcdPrint("num: 5");
+      break;
+    case 90:
+      lcdPrint("num: 6");
+      break;
+    case 66:
+      lcdPrint("num: 7");
+      break;
+    case 74:
+      lcdPrint("num: 8");
+      break;
+    case 82:
+      lcdPrint("num: 9");
+      break;
+    default:
+      lcd.clear();
+      lcd.print(IrReceiver.decodedIRData.command);
+      lcd.print(" other button");
+  }
 }
