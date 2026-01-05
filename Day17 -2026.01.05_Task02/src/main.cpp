e94f130917e0e876c6fa822df40b52b22b86f912
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-01-05 09:02:44 +0530

    Day17 -2026.01.05_Task02

--- Day17 -2026.01.05_Task02/src/main.cpp
+++ Day17 -2026.01.05_Task02/src/main.cpp
@@ -1,18 +1,45 @@
 #include <Arduino.h>
+#include <IRremote.hpp>
 
-// put function declarations here:
-int myFunction(int, int);
+#define IR_RECEIVE_PIN 2
+int greenLed = 12;
+int redLed=11;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  pinMode(greenLed, OUTPUT);
+  pinMode(redLed, OUTPUT);
+  
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+  Serial.println("IR Receiver Ready");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (IrReceiver.decode()) {
+    unsigned long irCode = IrReceiver.decodedIRData.decodedRawData;
+    
+    Serial.print("IR Code: 0x");
+    Serial.println(irCode, HEX);
+    
+    // Change these codes to match your remote buttons
+    if (irCode == 0xBA45FF00) {  // Green LED ON
+      digitalWrite(greenLed, HIGH);
+      Serial.println("Green LED ON");
+    }
+    else if (irCode == 0xB946FF00) {  // Green LED OFF
+      digitalWrite(greenLed, LOW);
+      Serial.println("Green LED OFF");
+    }
+    else if (irCode == 0xB847FF00) {  // Red LED ON
+      digitalWrite(redLed, HIGH);
+      Serial.println("Red LED ON");
+    }
+    else if (irCode == 0xBB44FF00) {  // Red LED OFF
+      digitalWrite(redLed, LOW);
+      Serial.println("Red LED OFF");
+    }
+    
+    IrReceiver.resume();
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
